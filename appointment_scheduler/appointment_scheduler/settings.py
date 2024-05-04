4f097cba7ae24586a48a284d954f67954b3f8dae
SoftwareTechnology-Hub <145540242+SoftwareTechnology-Hub@users.noreply.github.com>
2024-05-04 17:56:17 +0530

    Update settings.py

--- appointment_scheduler/appointment_scheduler/settings.py
+++ appointment_scheduler/appointment_scheduler/settings.py
@@ -132,3 +132,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ''  # Your SMTP server host
+EMAIL_PORT = 587  # Your SMTP server port (587 for TLS)
+EMAIL_USE_TLS = True  # Enable TLS (True or False)
+EMAIL_HOST_USER = ''  # Your SMTP username
+EMAIL_HOST_PASSWORD = ''  # Your SMTP password
+
